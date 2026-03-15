53f45da2cb5ded9e8e0e7bb7ad669437581f2aa8
ArthurFontis <arthurfontis17@gmail.com>
2026-03-15 12:46:09 -0300

    15/03/26

---
+++ aula01/aula01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+
+int main() {
+    int numero = 10;
+    char inicial = 'L';
+    float valor = 3.50;
+    printf("%0.2f", valor);
+    return 0;
+}
---
+++ aula02/aula02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 30
+
+int main()
+{
+    //const int max = 20;
+    printf("%d", MAX);
+
+
+    return 0;
+}
